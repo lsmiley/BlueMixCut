723a8fa7944859f21ba99b87d53f24ab300071bb
LARRY SMILEY <lsmiley@us.ibm.com>
2018-09-23 15:03:02 -0400

    Add project files.

---
+++ SizingToolNew2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SizingToolNew2.Startup))]
+namespace SizingToolNew2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
